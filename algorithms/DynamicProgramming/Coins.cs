3d1e544cd7f6b21df1d03fd47d4d8edbccbe88c7
khanh le <kle@kellyway.com>
2020-04-26 13:22:55 -0400

    Add coins problem

---
+++ algorithms/DynamicProgramming/Coins.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace algorithms.DynamicProgramming
+{
+    class Coins
+    {
+        public int GetMinCoins(int[] coins, int sum)
+        {
+            if (sum < 0 || coins == null || coins.Length == 0)
+                return 0;
+            var len = coins.Length;
+            int[,] a = new int[sum,len];
+            for (int i = 0; i < sum; i++)
+                a[i, 0] = int.MaxValue;
+            for(int i=1; i<sum; i++)
+            {
+                for(int j=0; j<len; j++)
+                {
+                    if (i >= coins[j])
+                    {
+                        int val = 0;
+                        if (i - coins[j] >= 0)
+                            val = a[i, j - 1] > a[i - coins[j], j - 1] ? a[i - coins[j], j - 1] : a[i, j - 1];
+                        else
+                            val = int.MaxValue;
+                        a[i, j] = val;
+                    }
+                    else
+                        a[i, j] = int.MaxValue;
+                }
+            }
+
+            Console.WriteLine("Output: ");
+            for(int i=0; i<sum; i++)
+            {
+                for (int j = 0; j < len; j++)
+                    Console.Write(a[i, j] + "  ");
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+            return 0;
+        } 
+    }
+}
---
+++ algorithms/Program.cs
@@ -0,0 +1,15 @@
+﻿using algorithms.DynamicProgramming;
+using System;
+
+namespace algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Coins().GetMinCoins(new int[] {1,3,5 },11);
+        }
+    }
+
+
+}
