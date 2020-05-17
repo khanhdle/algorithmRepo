ddfef4a720985df846a3f4e22f335862805d9cd9
khanhdle <khanh.dle@outlook.com>
2020-05-17 17:24:36 -0400

    add problem project

---
+++ problems/problems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
