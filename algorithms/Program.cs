cde44facf39207e3ad9cbb156d30414ac2cd88b4
Khanh Le <khanh.dle@outlook.com>
2020-12-09 15:16:05 -0500

    add another change 2 to Progarm.cs

--- algorithms/Program.cs
+++ algorithms/Program.cs
@@ -9,6 +9,7 @@ namespace algorithms
         {
             new Coins().GetMinCoins(new int[] {1,3,5 },11);//test merge fetch
             //add another change
+            //add another change 2
         }
     }
 
